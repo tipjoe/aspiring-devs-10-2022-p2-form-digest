0eed354228ed70f572fa27b6e053d132dbf38b4c
ojsung <jacob.sung@lendio.com>
2022-10-05 21:32:40 -0600

    Create second project server

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # aspiring-devs-p2
-Second project for mentees in the Aspiring Devs group
+Second project for mentees in the Aspiring Devs group to build.  Don't forget to talk to your mentor if you get stuck!
+
+## Objective
+Create a web app that can `CREATE` new stories, `READ` stories from the server, `UPDATE` them with changes, and `DELETE` stories when they're no longer wanted.
+
+The final design is up to you, but the stories should be easy to query and easy to read.
+
+## Getting Started
+To get started, 
+1. Clone this repository
+   - In your home folder (if you're on Windows, you can use your C: drive), create a `dev` folder, and open it.
+   - Open a terminal in this folder and type `git clone https://github.com/ojsung/aspiring-devs-p2.git`
+2. Open up `src/App.html` which has been provided to you in this project.  Follow the instructions contained there to install NodeJS and a code editor.
+3. Once NodeJS has been installed, run `npm run server` in the `aspiring-devs-p2` folder.
+   - This will start the server that you will interact with to make and retrieve stories
+4. Take a look at `src/services/http.service.js`.  This folder contains information about the server and how to interact with data through it.
+5. The rest is up to you.  Good luck. We're excited to see what you create.
---
+++ src/services/http.service.js
@@ -0,0 +1,87 @@
+const port = require('../../DO_NOT_TOUCH/server').port || 9219
+
+/**
+ * 
+ * For this class, feel free to install any package to handle making requests.  Axios is nice, as well as node_fetch.
+ * or if you're a real yippee zonkers ranger, you can also use NodeJS's built-in `http` package (don't actually do this)
+ * 
+ * The base URL has been provided to you (#baseUrl).  All other urls should build from it
+ * - ex. if your endpoint was /hello_world, you would use `const storyUrl = this.#baseUrl + '/hello_world'` 
+ * 
+ * There are two endpoints:
+ *  - /story
+ *  - /user_stories
+ *  
+ *  For these two endpoints, 
+ *  /story accepts GET and POST requests
+ *  /user_stories accepts GET requests
+ * 
+ * ## Endpoints:
+ * 
+ * ### The `/story` Endpoint
+ *  
+ * #### GET /story
+ * When this url sent a request, it will retreive, or READ, a list of all stories currently stored in the database
+ *   - The data shape is left up to the reader to discover _smirk_
+ * 
+ * #### GET /story/$storyId
+ * An optional parameter of storyId can be passed in.  If the user knows the id of a story, they can hit this endpoint to retrieve a specific story.
+ *    - When the database is in its pristine state, there will be only one story in the db.  And its id is `test-story-id`.  You can use this to begin testing out
+ *   how to build out this handler. (don't include the $ symbol)
+ *   
+ * #### POST /story
+ * To CREATE a new story in the database, a post request can be sent to the /story url.
+ *   - The body of the request should be as follows:
+ * 
+```
+  {
+    "story_title": "the title of your story",
+    "story_content": "your actual story. it should have a beginning, a middle, and end. maybe some plot. try to write a few thousand words. or pages.  it's up to you and your creativity. maybe this is the moment you discover that software isn't for you, and you were born to be a writer."
+  }
+```
+ *  - To ensure the story is attributed to the correct author, this endpoint and method requires an authentication header.
+ *     - For simplicity's sake, the authentication header value is just "username password".  
+ *     - There is a test user seeded to the database.  You can use this test user for this method.
+ *         - username: `test-user`
+ *         - password: `test`
+ *         
+ * #### PUT /story/$storyId
+ * To UPDATE an existing story in the database, a put request can be sent to the /story endpoint.
+ *  - The body of the request should be the same at POST /story
+ *  - An authentication header is also required for this endpoint, same as POST.
+ * - Again, when providinig the `storyId` don't include the `$`
+ *  
+ * #### DELETE /story/$storyId
+ * To DELETE an existing story in the database, a DELETE request can be sent to the /story/$storyId endpoint.
+ *  - There is no body for this endpoint
+ *  - An authentication header is required
+ *  - Are you sure you want to delete this story? (y/N)
+ * 
+ * ### The `/user_stories/$username` Endpoint
+ * This is an optional endpoint that you can implement if you'd like more practice.  Again, it is not expected that you implement this endpoint.  It's only here for additional practice if you want it.
+ *   - This endpoint will return all the stories stored in the database for the username provided.
+ *   - Only the GET method is available for this endpoint
+ *   - A reminder: when passing in the username, don't include the `$` symbol
+ *   
+ */
+
+class HttpService {
+    #baseUrl = `http://localhost:${port}`
+    get() {
+        throw new ReferenceError("NOT IMPLEMENTED")
+    }
+    
+    post() {
+        throw new ReferenceError("NOT IMPLEMENTED")
+    }
+    
+    put() {
+        throw new ReferenceError("NOT IMPLEMENTED")
+    }
+    
+    delete() {
+        throw new ReferenceError("NOT IMPLEMENTED")
+    }
+}
+
+module.exports = HttpService
